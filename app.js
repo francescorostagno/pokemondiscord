8f2104604ddcadbf12be8cd713efefd45d75e7db
Glitch (pokemondiscord) <none>
2023-02-23 09:51:56 +0000

    🌊🎂 Checkpoint ./app.js:32050870/17

--- app.js
+++ app.js
@@ -234,7 +234,8 @@ app.listen(3000, () => {
   console.log('Listening on port 3000');
 
   // Check if guild commands from commands.json are installed (if not, install them)
-  HasGuildCommands(process.env.APP_ID, process.env.GUILD_ID, [
+  
+(process.env.APP_ID, process.env.GUILD_ID, [
     TEST_COMMAND,
     CHALLENGE_COMMAND,
     RULES_COMMAND,
