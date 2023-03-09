8c8df12ab453e0b3a0b92feaeb8e225962b2c82f
francescorostagno <71463711+francescorostagno@users.noreply.github.com>
2023-03-09 10:13:23 +0100

    correzioni

--- app.js
+++ app.js
@@ -195,6 +195,15 @@ client.on("messageCreate",msg => {
 
 })
 
+client.on('interactionCreate', async interaction => {
+  if (!interaction.isCommand()) return;
+
+  if (interaction.commandName === 'ping') {
+    await interaction.reply('Pong!');
+  }
+});
+
+
 client.on('guildMemberRemove',(member) => {
   let goodbyembed = new Discord.MessageEmbed()
       .setAuthor(`${member.user.tag} just left!`, member.user.avatarURL())
