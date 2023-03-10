16777cbd33d6c113bdebfa323fe11660d12fec09
francescorostagno <71463711+francescorostagno@users.noreply.github.com>
2023-03-10 10:27:30 +0100

    correzioni

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config()
 import express from 'express';
 import {Client, Events, GatewayIntentBits} from "discord.js";
 import {InteractionResponseType, InteractionType, MessageComponentTypes,} from 'discord-interactions';
